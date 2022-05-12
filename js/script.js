b458b96727ed36218e54c0ad7c36e791f46f7b82
Joakim A <89795849+joakim-abra@users.noreply.github.com>
2022-05-12 11:23:50 +0200

    finish`?

--- js/script.js
+++ js/script.js
@@ -5,30 +5,47 @@ const tasks = document.getElementById("tasks")
 // getTasks
 const getTasks = () => {
     let allTasks = localStorage.getItem("taskList")
+    if(allTasks!="")
+    {
     allTasks = allTasks.split(",")
     console.log(allTasks)
-    tasks.innerHTML= allTasks.map((task) => `
+        tasks.innerHTML= allTasks.map((task,index) => `
         <div class="task">
         <div class="content">
         <input class="text type="text" readonly="readonly" value="${task}">
         </div>
         <div class="actions">
-        <button class="delete">Delete</button>
+        <button class="delete" onclick="deleteTask(${index})">Delete</button>
         </div>
         </div>
         `)
         .join("")
+    }
+    else
+    {
+        tasks.innerHTML=`
+        <div class="no-tasks-found">
+        <h2>No tasks found</h2>
+        </div>`
+    }
 }
-
+        
 // addTask
 const addTask = (task) =>{
     let allTasks = localStorage.getItem("taskList")
-    allTasks = allTasks ? allTasks.split(".") : []
+    allTasks = allTasks ? allTasks.split(",") : []
     allTasks.push(task)
     localStorage.setItem("taskList",allTasks)
 }
 
 // deleteTask
+const deleteTask = (index) => {
+    let allTasks = localStorage.getItem("taskList")
+    allTasks = allTasks.split(",")
+    allTasks.splice(index,1)
+    localStorage.setItem("taskList",allTasks)
+    getTasks()
+}
 
 // Form Submit
 form.onsubmit = (event) => {
